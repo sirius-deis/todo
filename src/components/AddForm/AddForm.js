cea6eddd3d628ba5feb7f540220c3f9d891a7cfa
Sirius Deis <falebsy@gmail.com>
2022-04-24 16:36:28 +0200

    add AddForm component styles

--- src/components/AddForm/AddForm.js
+++ src/components/AddForm/AddForm.js
@@ -3,10 +3,10 @@ import './AddForm.css';
 const AddForm = () => {
     return (
         <form className='addForm'>
-            <input type="text" placeholder='Add new...'/>
-            <label htmlFor='date'>
+            <input type="text" placeholder='Add new...' className='title'/>
+            <label htmlFor='date' className='data_picker'>
                 <input type="date" id='date' className='date'/>
-                <i className="fa-solid fa-calendar-days"></i>
+                <i className="fa-solid fa-calendar-days toggle"></i>
             </label>
             <button type='submit' className='btn'>add</button>
         </form>
