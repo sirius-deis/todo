0aa7529ef8b3fedf8d793016288d8bd3b54288df
Sirius Deis <falebsy@gmail.com>
2022-04-24 16:00:22 +0200

    add Header component styles

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className='header'>
             <i className="fa-solid fa-square-check header__logo"></i>
-            <h1 className='heading'>My Todo-s</h1>
+            <h1 className='header__heading'>My Todo-s</h1>
         </div>
     );
 }
